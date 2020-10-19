73b1bb13d74fd4edb96cc359c4e8cf727a1d4104
hartlomiej <bartolini801@gmail.com>
2020-10-19 14:55:05 +0200

    Create ratings.bookmark.js

---
+++ bookmarks/ratings.bookmark.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bcheckboxes %3D document.getElementsByClassName("ui-checkboxradio ui-helper-hidden-accessible")%3Bfor(let x %3D 1%3B x < 10%3B x%2B%2B)%7Bfor(let i %3D 0%3B i < checkboxes.length%3B i%2B%2B)if(Math.random() > 0.8)checkboxes%5Bi%5D.click()%3B%7D%7D)()
